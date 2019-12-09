355dba30a787f23e84dea860705a6b9178f18686
Suriya Suriyakanthan <suriya.suriyakanthan@stud.hslu.ch>
2019-12-09 13:21:06 +0100

    Projekt v_2

---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Pointer : MonoBehaviour
+{
+    public float m_DefaultLenght = 5.0f;
+    public GameObject m_Dot;
+    public VRInputModule m_InputModule;
+
+    private LineRenderer m_LineRenderer = null;
+
+    private void Awake()
+    {
+        m_LineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        UpdateLine();
+    }
+
+    private void UpdateLine()
+    {
+        PointerEventData data = m_InputModule.GetData();
+        float targetLenght = data.pointerCurrentRaycast.distance == 0 ? m_DefaultLenght : data.pointerCurrentRaycast.distance;
+
+        RaycastHit hit = CreateRaycast(targetLenght);
+
+        Vector3 endPosition = transform.position + (transform.forward * targetLenght);
+
+        if(hit.collider != null)
+            endPosition = hit.point;
+
+        m_Dot.transform.position = endPosition;
+
+        m_LineRenderer.SetPosition(0, transform.position);
+        m_LineRenderer.SetPosition(1, endPosition);
+    }
+
+    private RaycastHit CreateRaycast(float lenght)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit, m_DefaultLenght);
+
+        return hit;
+    }
+}
